92fccd9006b6e18bed61e224ee14cacc3d9112b2
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-11-10 15:25:05 +0000

    Code Challenge 14 completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+const stripUpperCase = str => {
+   let rmvUpCase = "";
+   for (let i of str) {
+       if (i == i.toLowerCase()) {
+           rmvUpCase += i ;
+       }
+   }
+   
+   return rmvUpCase;
+};
+
+console.log(stripUpperCase('Hello!'));
